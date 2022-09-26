a5e195373e9f4b34a033b8c3daf3e9c8f9c87b96
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-09-26 11:14:04 +0200

    feat: Admin authorization

--- GruppÖvningSemester/Controllers/AllocationController.cs
+++ GruppÖvningSemester/Controllers/AllocationController.cs
@@ -1,4 +1,5 @@
 ﻿using GruppÖvningSemester.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace GruppÖvningSemester.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AllocationController : Controller
     {
         private readonly IRepository<Allocation> _repository;
@@ -16,9 +18,18 @@ namespace GruppÖvningSemester.Controllers
             _repository = repository;
         }
 
-        //public ViewResult DaysLeft()
-        //{
-        //    _repository.GetById();
-        //}
+        public ActionResult Index()
+        {
+            var allAllocations = _repository.GetAll();
+            return View(allAllocations);
+        }
+        //=>
+        //Content("Admin")
+
+        public ActionResult DaysLeft(int id)
+        {
+            var foundAllocation = _repository.GetById(id);
+            return View(foundAllocation);
+        }
     }
 }
